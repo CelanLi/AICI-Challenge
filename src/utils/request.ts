795a49c207e15467d3c56d9e6687762ed4d7418f
Chen <celan.li@tum.de>
2024-07-13 12:03:11 +0200

    first commit

---
+++ src/utils/request.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+import exp from "constants";
+
+const API =  "https://jsonplaceholder.typicode.com/users"
+// create axios request instance
+const serviceAxios = axios.create({
+    baseURL: API, // base request address
+    timeout: 10000,
+    withCredentials: false,
+});
+
+export default serviceAxios;
